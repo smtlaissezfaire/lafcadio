7e92700e3e12346eb7c77bb0aeaa7ef4e76037cb
fhwang <fhwang>
2003-07-01 14:36:00 +0000

    extended sprintf-style support

--- lafcadio/dateTime/DateFormatter.rb
+++ lafcadio/dateTime/DateFormatter.rb
@@ -9,18 +9,40 @@ class DateFormatter
 				'August', 'September', 'October', 'November', 'December' ][month - 1]
 	end
 
+	def dayName (date)
+		[ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+				'Saturday' ][ date.wday ]
+	end
+	
+	def monthAbbrev (date)
+		[ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+				'Nov', 'Dec' ][ date.mon - 1 ]
+	end
+	
+	def getTime (date)
+		date.type == Date ? DateTimeUtil.dateToTime (date) : date
+	end
+
 	def to_s (date)
 		if date
 			str = @format.clone
 			if @sprintfFormat
 				sprintfArgs = []
-				str.gsub!(/(yyyy|mm|dd)/) { |match|
+				str.gsub!(/(yyyy|yy|mm|dd|hh|mn|ss)/) { |match|
 					if match == 'yyyy'
 						nextArg = date.year
+					elsif match == 'yy'
+						nextArg = date.year.divmod(100)[1]
 					elsif match == 'mm'
 						nextArg = date.month
 					elsif match == 'dd'
 						nextArg = date.day
+					elsif match == 'hh'
+						nextArg = getTime(date).hour
+					elsif match == 'mn'
+						nextArg = getTime(date).min
+					elsif match == 'ss'
+						nextArg = getTime(date).sec
 					end
 					sprintfArgs << nextArg
 					'd'
@@ -30,8 +52,10 @@ class DateFormatter
 				str = str.gsub /yyyy/, date.year.to_s
 				str = str.gsub /mm/, date.month.to_s
 				str = str.gsub /dd/, date.day.to_s
-				str = str.gsub /month/, monthName(date.month)
 			end
+			str = str.gsub /month/, monthName(date.month)
+			str = str.gsub /day/, dayName(date)
+			str = str.gsub /mon/, monthAbbrev(date)
 		else
 			str = ''
 		end
--- test/dateTime/DateFormatter.rb
+++ test/dateTime/DateFormatter.rb
@@ -27,5 +27,10 @@ class TestDateFormatter < LafcadioTestCase
 		assert_equal '', formatter.to_s (nil)
 		assert_equal '2002 10 28', formatter.to_s (@oct28_2002)
 		assert_equal '2002 11 03', formatter.to_s (@nov3_2002)
+		formatter2 = DateFormatter.new (
+				'day, %02dd-mon-%02yy %02hh:%02mn:%02ss GMT')
+		nov9_1999_23_12_40 = Time.gm (1999, 'nov', 9, 23, 12, 40)
+		assert_equal "Tuesday, 09-Nov-99 23:12:40 GMT",
+				formatter2.to_s(nov9_1999_23_12_40)
 	end
 end
