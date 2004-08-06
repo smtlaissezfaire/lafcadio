30c2adc43117357f6bacf1c722a95a795e88690c
francis <francis>
2004-08-06 02:24:39 +0000

    removed PasswordField#maxLength, #autoGenerate

--- lib/lafcadio/objectField/PasswordField.rb
+++ lib/lafcadio/objectField/PasswordField.rb
@@ -12,18 +12,5 @@ module Lafcadio
 			0.upto(8) { |i| value += chars[rand(chars.size)] }
 			value
 		end
-
-		attr_reader :maxLength
-		attr_accessor :autoGenerate
-
-		# [objectType]  The domain class that this object field belongs to.
-		# [name]        The name of this field.
-		# [maxLength]   The maximum length of the password. (Deprecated)
-		# [englishName] The descriptive English name of this field. (Deprecated)
-		def initialize(objectType, maxLength, name = "password", englishName = nil)
-			super(objectType, name, englishName)
-			@maxLength = maxLength
-			@autoGenerate = true
-		end
 	end
 end
