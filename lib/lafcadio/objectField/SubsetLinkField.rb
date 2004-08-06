a3ce91759b81945cf45903f52461f1dd44602660
francis <francis>
2004-08-06 02:30:35 +0000

    cleanup

--- lib/lafcadio/objectField/SubsetLinkField.rb
+++ lib/lafcadio/objectField/SubsetLinkField.rb
@@ -2,18 +2,18 @@ require 'lafcadio/objectField/LinkField'
 
 module Lafcadio
 	class SubsetLinkField < LinkField #:nodoc:
-		def SubsetLinkField.instantiationParameters( fieldElt )
-			parameters = super( fieldElt )
-			parameters['subsetField'] = fieldElt.attributes['subsetField']
-			parameters
-		end
-		
-		def SubsetLinkField.instantiateWithParameters( domainClass, parameters )
+		def self.instantiateWithParameters( domainClass, parameters )
 			self.new( domainClass, parameters['linkedType'],
 			          parameters['subsetField'], parameters['name'],
 								parameters['englishName'] )
 		end
 
+		def self.instantiationParameters( fieldElt )
+			parameters = super( fieldElt )
+			parameters['subsetField'] = fieldElt.attributes['subsetField']
+			parameters
+		end
+		
 		attr_accessor :subsetField
 
 		def initialize(objectType, linkedType, subsetField,
