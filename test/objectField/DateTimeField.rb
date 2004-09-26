b56acb3a97511eb9c7d0605ea7402719f4660b94
francis <francis>
2004-09-26 22:55:24 +0000

    reordered test methods

--- test/objectField/DateTimeField.rb
+++ test/objectField/DateTimeField.rb
@@ -8,6 +8,11 @@ class TestDateTimeField < LafcadioTestCase
 		@aug24 = Time.local(2002, "aug", 24, 13, 8, 22)
 	end
 
+	def testValueForSQL
+		assert_equal "'2002-08-24 13:08:22'",(@dateTimeField.valueForSQL(@aug24))
+		assert_equal "null", @dateTimeField.valueForSQL(nil)
+	end
+
 	def testValueFromSQL
 		ts1 = DBI::Timestamp.new( 2002, 8, 24, 13, 8, 22 )
 		value = @dateTimeField.valueFromSQL ts1, false
@@ -18,10 +23,5 @@ class TestDateTimeField < LafcadioTestCase
 		dbi_oct6 = DBI::Timestamp.new( 2002, 10, 6 )
 		assert_equal oct6,(@dateTimeField.valueFromSQL dbi_oct6)
 		assert_equal nil,(@dateTimeField.valueFromSQL nil)
-	end
-
-	def testValueForSQL
-		assert_equal "'2002-08-24 13:08:22'",(@dateTimeField.valueForSQL(@aug24))
-		assert_equal "null", @dateTimeField.valueForSQL(nil)
 	end
 end
